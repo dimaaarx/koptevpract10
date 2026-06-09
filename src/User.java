59232974e3d9a7c35934f0b3eecf7e7643090d01
dimaaarx <koptevdima1107@gmail.com>
2026-06-09 23:53:59 +0300

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,4 +1,66 @@
-package PACKAGE_NAME;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class User {
+    private int id;
+    private String name;
+    private String password;
+    private LocalDateTime lastLoginDate;
+    private boolean isLoggedIn;
+
+    public User(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.lastLoginDate = null;
+        this.isLoggedIn = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
+    }
+
+    public void login() {
+        this.isLoggedIn = true;
+        this.lastLoginDate = LocalDateTime.now();
+    }
+
+    public void logout() {
+        this.isLoggedIn = false;
+    }
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public LocalDateTime getLastLoginDate() {
+        return lastLoginDate;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        User user = (User) obj;
+        return Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
